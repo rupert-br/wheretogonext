176a121c0cfdc7824a2fad90f9efee41bca0d9d1
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2021-09-12 11:11:38 +0200

    Added external api call

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+require('dotenv').config()
 
 const app = express();
 
@@ -10,10 +12,26 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/get_weather/:one/:two/:radius', function (req, res) {
-    res.send(req.params);
+app.get('/get_weather/:lat/:lon/:radius', function (req, res) {
+    let lat = req.params.lat;
+    let lon = req.params.lon;
+
+    console.log(lat, lon);
+
+    let API_key = process.env.API_KEY;
+    let url_weather = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&appid=${API_key}`
+    console.log(url_weather);
+
+    axios({
+        method: 'get',
+        url: url_weather
+      })
+      .then(response => {
+          res.send(response.data.current);
+      })
+
   });
 
-app.listen(3000);
+app.listen(3030);
 
-console.log('Server started at http://localhost:3000');
+console.log('Server started at http://localhost:3030');
--- public/js/getWeather.js
+++ public/js/getWeather.js
@@ -1,7 +1,13 @@
 function getWeather(){
-    var one = document.getElementById('inputEmail4');
-    let url = '/get_weather'
+    let lat = document.getElementById('lat').value;
+    let lon = document.getElementById('lon').value;
+    let radius = document.getElementById('radius').value;
+
+    let url = `/get_weather/${lat}/${lon}/${radius}`;
     fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data => {
+            console.log(data)
+            document.getElementById('results').innerHTML = 'The current temperature is ' + data.temp + ' °C';
+        });
 }
