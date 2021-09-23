1b3d7666a1b18606f0c085da680ccc20789b045f
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2021-09-23 22:08:38 +0200

    changed port number

--- index.js
+++ index.js
@@ -54,6 +54,6 @@ app.get('/get_weather/:lat/:lon/:radius', function (req, res) {
 
   });
 
-app.listen(3030);
+app.listen(process.env.PORT || 5000);
 
-console.log('Server started at http://localhost:3030');
+console.log('Server started at http://localhost:5000');
