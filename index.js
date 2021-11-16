ef4a5bb536f6c84170526c1eb9eaf14d6e7f5442
Rupert Brandstaetter <rupert.brandstaetter@gmail.com>
2021-11-16 18:58:02 +0100

    added front end

--- index.js
+++ index.js
@@ -107,6 +107,7 @@ app.get('/get_city_by_posititon/:lat/:lon', function(req, res){
 })
 
 app.get('/get_city/:searchStr', function(req, res) {
+    console.log('api called')
     let searchStr = req.params.searchStr;
     // const url_city = `https://warnungen.zamg.at/wsapp/api/searchLocation?s=${searchStr}`
     const url_city = `https://nominatim.openstreetmap.org/search.php?q=${searchStr}&format=json&countrycodes=at`
